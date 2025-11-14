8187f478f44838c465ca057359c7a87db1538e92
aswin7469 <aswinas@pixysos.com>
2025-11-15 02:42:12 +0530

    Settings: Drop GPSU entry from firmware version
    * I m too poor to get partner access and too lazy to do prebuilts

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -74,7 +74,6 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
             +MaintainerPreference()
             +SecurityPatchLevelPreference()
             +LineageVendorSecurityPatchLevelPreference()
-            +MainlineModuleVersionPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
             +LineageBuildDatePreference()
